1211c5cd53d2a9d39ec4b1ae5eb28a4517ad43ba
ihorserba <ihorserba@gmail.com>
2020-08-03 18:34:17 +0300

    Implemented networking part of streaming logic for .NET client

--- examples/stream.cs
+++ examples/stream.cs
@@ -3,25 +3,31 @@
     using System;
     using System.IO;
     using System.Collections.Generic;
-    
+    using System.Net.WebSockets;
+    using System.Threading;
+    using System.Text.Json;
+    using System.Buffers.Binary;
+    using System.Diagnostics;
+    using Microsoft.Extensions.Configuration;
+
     // https://tools.ietf.org/html/rfc7845
     // https://tools.ietf.org/html/rfc3533
-    class OpusFileStream {
+    class OpusFileStream : IDisposable {
 
-        Stream opusfile;
+        Stream opusFile;
         byte[] segmentSizes;
         byte segmentIdx;
         byte segmentsCount;
         UInt32 sequenceNumber;
-        UInt32 sampleRate;
+        public UInt32 sampleRate { get; private set; }
         int opusHeadersCount;
-        float packetDuration;
-        int framesPerPacket;
+        public int packetDuration { get; private set; }
+        public int framesPerPacket { get; private set; }
         List<byte[]> savedPackets;
 
         public OpusFileStream(string filename) {
             try {
-                this.opusfile = File.OpenRead(filename);
+                this.opusFile = File.OpenRead(filename);
             } catch {
                 throw new Exception("Failed opening " + filename);
             }
@@ -32,21 +38,33 @@
             }
         }
 
+        public void Dispose() {
+            if (this.opusFile is null) {
+                return;
+            }
+            this.opusFile.Dispose();
+        }
+
         private bool getNextOggPacketStart() {
             // Each Ogg page starts with magic bytes "OggS"
             // Stream may be corrupted, so find a first valid magic
             int verifiedBytes = 0;
             byte[] magic = System.Text.Encoding.UTF8.GetBytes("OggS");
             byte[] onebyte = new byte[1];
-            while (this.opusfile.Read(onebyte, 0, 1) == 1) {
-                if (onebyte[0] == magic[verifiedBytes]) {
-                    verifiedBytes++;
-                    if (verifiedBytes == 4) {
-                        return true;
+            try {
+                while (this.opusFile.Read(onebyte, 0, 1) == 1) {
+                    if (onebyte[0] == magic[verifiedBytes]) {
+                        verifiedBytes++;
+                        if (verifiedBytes == 4) {
+                            return true;
+                        }
+                    } else {
+                        verifiedBytes = 0;
                     }
-                } else {
-                    verifiedBytes = 0;
                 }
+            } catch {
+                Console.WriteLine("Failed reading media while looking for next Ogg packet");
+                return false;
             }
             return false;
         }
@@ -73,7 +91,12 @@
             // | ...                                                           | 28-
             // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
             byte[] header = new byte[23];
-            if (this.opusfile.Read(header, 0, header.Length) != header.Length) {
+            try {
+                if (this.opusFile.Read(header, 0, header.Length) != header.Length) {
+                    return false;
+                }
+            } catch {
+                Console.WriteLine("Failed reading media while parsing Ogg packet header");
                 return false;
             }
 
@@ -94,7 +117,12 @@
             this.segmentIdx = 0;
             if (this.segmentsCount > 0) {
                 this.segmentSizes = new byte[this.segmentsCount];
-                if (this.opusfile.Read(this.segmentSizes, 0, this.segmentsCount) != this.segmentsCount) {
+                try {
+                    if (this.opusFile.Read(this.segmentSizes, 0, this.segmentsCount) != this.segmentsCount) {
+                        return false;
+                    }
+                } catch {
+                    Console.WriteLine("Failed reading media while fetching Ogg page segment table");
                     return false;
                 }
             }
@@ -112,8 +140,13 @@
             while (this.segmentIdx < this.segmentsCount) {
                 byte segmentSize = this.segmentSizes[this.segmentIdx];
                 byte[] segment = new byte[segmentSize];
-                if (this.opusfile.Read(segment, 0, segmentSize) != segmentSize) {
-                    break;
+                try {
+                    if (this.opusFile.Read(segment, 0, segmentSize) != segmentSize) {
+                        break;
+                    }
+                } catch {
+                    Console.WriteLine("Failed reading media while fetching Ogg page segment");
+                    return (null, false);
                 }
 
                 byte[] newdata = new byte[data.Length + segmentSize];
@@ -198,7 +231,7 @@
             return true;
         }
 
-        private (int, float) parseOpusToc(byte[] data) {
+        private (int, int) parseOpusToc(byte[] data) {
             // https://tools.ietf.org/html/rfc6716#section-3.1
             // Each Opus packet starts with the Table Of Content Byte:
             // |0 1 2 3 4 5 6 7| Bit
@@ -229,7 +262,8 @@
             byte config = (byte)((data[0] >> 3) & 0x1f);
             for (int i = 0; i < configsMS.Count; i++) {
                 if (Array.Exists(configsMS[i], conf => (conf == config))) {
-                    return (framesPerPacket, durations[i]);
+                    // The packet duration 2.5ms currently is not supported
+                    return (framesPerPacket, (int)durations[i]);
                 }
             }
             return (framesPerPacket, 20);
@@ -292,7 +326,7 @@
                 }
 
                 // Verify the Opus TOC is the same as we initially declared
-                (int frames, float duration) = this.parseOpusToc(data);
+                (int frames, int duration) = this.parseOpusToc(data);
                 if (this.framesPerPacket != frames || this.packetDuration != duration) {
                     data = new byte[0];
                     Console.WriteLine("Skipping frame - TOC differs");
@@ -331,6 +365,279 @@
             }
             return true;
         }
+    }
 
+    class ZelloMediaStream : IDisposable {
+        private OpusFileStream opusStream;
+        private IConfigurationRoot configuration;
+        private CancellationTokenSource networkingCancelation;
+        private ClientWebSocket webSocket;
+        private UInt32 streamId;
+        private UInt32 packetId;
+        byte[] rcvBuffer;
+        Stopwatch stopWatch;
+        const string url = "wss://zello.io/ws";
+        const int timeoutMS = 5000;
+
+        public class ResponseJson {
+            public string command { get; set; }
+            public string status { get; set; }
+            public string refresh_token { get; set; }
+            public bool success { get; set; }
+            public UInt32 stream_id { get; set; }
+        };
+
+        public ZelloMediaStream(IConfigurationRoot config) {
+            this.configuration = config;
+            this.webSocket = new ClientWebSocket();
+            this.networkingCancelation = new CancellationTokenSource();
+            this.stopWatch = new Stopwatch();
+            this.rcvBuffer = new byte[1024];
+            this.webSocket.Options.SetBuffer(this.rcvBuffer.Length, this.rcvBuffer.Length,
+                this.rcvBuffer);
+            this.webSocket.Options.KeepAliveInterval = new TimeSpan(0, 0, 10);
+            this.opusStream = new OpusFileStream(config["media:filename"]);
+        }
+
+        public void Dispose() {
+            if (this.opusStream != null) {
+                this.opusStream.Dispose();
+            }
+            if (this.webSocket.State == WebSocketState.Open) {
+                this.stopStream();
+                networkingCancelation.Cancel(false);
+            }
+        }
+
+        public static bool checkConfiguration(IConfigurationRoot config) {
+            return config.GetSection("zello:username").Exists() &&
+                config.GetSection("zello:password").Exists() &&
+                config.GetSection("zello:token").Exists() &&
+                config.GetSection("zello:channel").Exists() &&
+                config.GetSection("media:filename").Exists();
+        }
+
+        public bool authenticate() {
+            try {
+                if (!this.webSocket.ConnectAsync(new Uri(ZelloMediaStream.url),
+                    this.networkingCancelation.Token).Wait(ZelloMediaStream.timeoutMS,
+                    this.networkingCancelation.Token))
+                {
+                    Console.WriteLine("Failed to connect to Zello server");
+                    return false;
+                }
+
+                var logonJson = new {
+                    seq = 1,
+                    command = "logon",
+                    username = this.configuration["zello:username"],
+                    password = this.configuration["zello:password"],
+                    auth_token = this.configuration["zello:token"],
+                    channel = this.configuration["zello:channel"]
+                };
+                byte[] logonCmd = System.Text.Encoding.UTF8.GetBytes(JsonSerializer.Serialize(logonJson));
+                bool isAuthorized = false;
+                bool isChannelAvailable = false;
+
+                webSocket.SendAsync(logonCmd, WebSocketMessageType.Text, true, this.networkingCancelation.Token).
+                    Wait(timeoutMS, this.networkingCancelation.Token);
+                 
+                while (!isAuthorized || !isChannelAvailable) {
+                    Array.Clear(this.rcvBuffer, 0, this.rcvBuffer.Length);
+                    if (!webSocket.ReceiveAsync(this.rcvBuffer, this.networkingCancelation.Token).
+                        Wait(ZelloMediaStream.timeoutMS, this.networkingCancelation.Token))
+                    {
+                        Console.WriteLine("Failed to get response from server");
+                        return false;
+                    }
+
+                    string responseStr = System.Text.Encoding.UTF8.GetString(this.rcvBuffer).TrimEnd('\0');
+                    ResponseJson responseJson = new ResponseJson();
+
+                    try {
+                        responseJson = JsonSerializer.Deserialize<ResponseJson>(responseStr);
+                    } catch {
+                        Console.WriteLine("Failed to parse json response");
+                        continue;
+                    }
+
+                    if (!String.IsNullOrEmpty(responseJson.refresh_token)) {
+                        isAuthorized = true;
+                    } else if (responseJson.command == "on_channel_status" && responseJson.status == "online") {
+                        isChannelAvailable = true;
+                    }
+                }
+            } catch {
+                Console.WriteLine("Failed to communicate with Zello server");
+                return false;
+            } 
+            return true;
+        }
+
+        public bool startStream() {
+            byte[] codecHeaderRaw = new byte[4];
+            byte packetDuration = (byte)this.opusStream.packetDuration;
+            byte framesPerPacket = (byte)this.opusStream.framesPerPacket;
+            UInt16 sampleRate = (UInt16)this.opusStream.sampleRate;
+
+            // sampleRate is represented in two bytes in little endian.
+            // https://github.com/zelloptt/zello-channel-api/blob/409378acd06257bcd07e3f89e4fbc885a0cc6663/sdks/js/src/classes/utils.js#L63
+            BinaryPrimitives.TryWriteUInt16LittleEndian(codecHeaderRaw, sampleRate);
+            codecHeaderRaw[2] = (byte)this.opusStream.framesPerPacket;
+            codecHeaderRaw[3] = packetDuration;
+
+            string codecHeader = Convert.ToBase64String(codecHeaderRaw);
+            var startStreamJson = new {
+                seq = 2,
+                command = "start_stream",
+                type = "audio",
+                codec = "opus",
+                codec_header = codecHeader,
+                packet_duration = packetDuration,
+            };
+            byte[] startStreamCmd = System.Text.Encoding.UTF8.
+                GetBytes(JsonSerializer.Serialize(startStreamJson));
+
+            this.webSocket.
+                SendAsync(startStreamCmd, WebSocketMessageType.Text, true, this.networkingCancelation.Token).
+                Wait(ZelloMediaStream.timeoutMS, this.networkingCancelation.Token);
+            
+            Array.Clear(this.rcvBuffer, 0, this.rcvBuffer.Length);
+            if (!this.webSocket.
+                ReceiveAsync(this.rcvBuffer, this.networkingCancelation.Token).
+                Wait(ZelloMediaStream.timeoutMS, this.networkingCancelation.Token))
+            {
+                Console.WriteLine("Failed to get response from Zello server");
+                return false;
+            }
+
+            string responseStr = System.Text.Encoding.UTF8.GetString(this.rcvBuffer).TrimEnd('\0');
+            ResponseJson responseJson = new ResponseJson();
+
+            try {
+                responseJson = JsonSerializer.Deserialize<ResponseJson>(responseStr);
+            } catch {
+                Console.WriteLine("Failed to parse json response");
+                return false;
+            }
+
+            if (responseJson.success && responseJson.stream_id != 0) {
+                this.streamId = responseJson.stream_id;
+                return true;
+            }
+            return false;            
+        }
+
+        private byte[] getNextStreamPacket() {
+            byte[] opusData = this.opusStream.GetNextOpusPacket();
+            byte[] streamIdRaw = new byte[4];
+            byte[] packetIdRaw = new byte[4];
+
+            if (opusData is null) {
+                return null;
+            }
+            this.packetId++;
+            byte[] streamPacket = new byte[opusData.Length + 9];
+            BinaryPrimitives.TryWriteUInt32BigEndian(streamIdRaw, this.streamId);
+            BinaryPrimitives.TryWriteUInt32BigEndian(packetIdRaw, this.packetId);
+
+            streamPacket[0] = 1;
+            streamIdRaw.CopyTo(streamPacket, 1);
+            packetIdRaw.CopyTo(streamPacket, 5);
+            opusData.CopyTo(streamPacket, 9);
+            return streamPacket;            
+        }
+
+        public bool sendAudio() {
+            try {
+                // Listen on the opened websocket, connection may be closed otherwise.
+                this.webSocket.ReceiveAsync(rcvBuffer, this.networkingCancelation.Token);
+            } catch (Exception e) {
+                Console.WriteLine("Unable to listen on websocket: " + e.Message);
+                return false;
+            }
+
+            while (true) {
+                this.stopWatch.Reset();
+                this.stopWatch.Start();
+                byte[] packet = this.getNextStreamPacket();
+                if (packet is null) {
+                    break;
+                }
+                try {
+                    this.webSocket.
+                        SendAsync(packet, WebSocketMessageType.Binary, true, this.networkingCancelation.Token).
+                        Wait(this.opusStream.packetDuration, this.networkingCancelation.Token);
+                } catch (Exception e) {
+                    Console.WriteLine("Got error while sending audio: " + e);
+                    return false;
+                }
+                this.stopWatch.Stop();
+                if (this.opusStream.packetDuration > this.stopWatch.ElapsedMilliseconds) {
+                    Thread.Sleep(this.opusStream.packetDuration - (int)this.stopWatch.ElapsedMilliseconds);
+                }
+            }
+            return true;
+        }
+
+        public bool stopStream() {
+            var stopStreamJson = new {
+                command = "stop_stream",
+                stream_id = this.streamId,
+            };
+            byte[] stopStreamCmd = System.Text.Encoding.UTF8.GetBytes(JsonSerializer.Serialize(stopStreamJson));
+            return this.webSocket.
+                SendAsync(stopStreamCmd, WebSocketMessageType.Text, true, this.networkingCancelation.Token).
+                Wait(ZelloMediaStream.timeoutMS, this.networkingCancelation.Token);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfigurationRoot config = null;
+
+            try {
+                config = new ConfigurationBuilder().
+                    SetBasePath(Directory.GetCurrentDirectory()).
+                    AddIniFile("stream.conf").Build();
+            } catch {
+                Console.WriteLine("Failed to open a config file");
+                return;
+            }
+            
+            if (!ZelloMediaStream.checkConfiguration(config)) {
+                Console.WriteLine("Invalid config file. See example");
+                return;
+            }
+
+            ZelloMediaStream zelloMediaStream = null;
+            try {
+                zelloMediaStream = new ZelloMediaStream(config);
+
+                if (!zelloMediaStream.authenticate()) {
+                    Console.WriteLine("Failed to authenticate");
+                    return;
+                }
+                if (!zelloMediaStream.startStream()) {
+                    Console.WriteLine("Failed to start streaming");
+                    return;
+                }
+                if (!zelloMediaStream.sendAudio()) {
+                    Console.WriteLine("Error during sending audio");
+                }
+                if (!zelloMediaStream.stopStream()) {
+                    Console.WriteLine("Failed to send stop stream request");
+                }
+            } catch (Exception e){
+                Console.WriteLine("Got an error: " + e.Message);
+                return;
+            } finally {
+                if (zelloMediaStream != null) {
+                    zelloMediaStream.Dispose();
+                }
+            }
+        }
     }
 }
