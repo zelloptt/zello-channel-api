d834451b34de246a5b849cf6fec3441ec1d3d3e3
Daniel Hunt <email@danielhunt.info>
2020-09-10 10:52:36 -0400

    feat: add error codes to constants

--- sdks/js/src/classes/constants.js
+++ sdks/js/src/classes/constants.js
@@ -29,6 +29,9 @@ module.exports = {
 
   ERROR_TYPE_UNKNOWN_SERVER_ERROR: 'Unknown server error',
   ERROR_TYPE_CONFIGURATION: 'configuration',
+  ERROR_TYPE_NOT_AUTHORIZED: 'not authorized',
+  ERROR_TYPE_INVALID_PASSWORD: 'invalid password',
+  ERROR_TYPE_INVALID_USERNAME: 'invalid username',
 
   EVENT_ERROR: 'error',
   EVENT_CONNECT: 'connect',
