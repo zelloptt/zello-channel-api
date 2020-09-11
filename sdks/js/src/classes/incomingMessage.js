c02713df55fecda1d8b10ba2ba314a658d6ed108
ihorserba <ihorserba@gmail.com>
2020-09-12 01:01:38 +0300

    Set flush time as LCM of possible packet durations

--- sdks/js/src/classes/incomingMessage.js
+++ sdks/js/src/classes/incomingMessage.js
@@ -26,7 +26,7 @@ class IncomingMessage extends Emitter {
         encoding: '32bitFloat',
         channels: 1,
         sampleRate: IncomingMessage.detectSampleRate(this.codecDetails.rate),
-        flushingTime: this.codecDetails.frameSize * 10,
+        flushingTime: 240,
       },
       session.options,
       {messageData: messageData}
