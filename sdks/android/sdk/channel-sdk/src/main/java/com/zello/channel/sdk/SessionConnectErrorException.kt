6238792281e43925eb081874e39e91149c248a58
Greg Cooksey <greg@zello.com>
2021-04-29 16:12:00 -0500

    Updates Android demo app to load sdk from Maven Central (#141)
    * Removes stale SDK sources from this repository
    * Updates build tools

--- sdks/android/sdk/channel-sdk/src/main/java/com/zello/channel/sdk/SessionConnectErrorException.kt
+++
@@ -1,3 +0,0 @@
-package com.zello.channel.sdk
-
-internal class SessionConnectErrorException(val error: SessionConnectError) : Exception(error.toString())
--- sdks/android/sdk/channel-sdk/src/main/java/com/zello/channel/sdk/platform/PlayerListener.kt
+++
@@ -1,7 +0,0 @@
-package com.zello.channel.sdk.platform
-
-internal interface PlayerListener {
-
-	fun onPlayerPositionChanged(pos: Int, Object: Any?)
-
-}
