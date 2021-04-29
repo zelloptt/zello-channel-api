2c29625f8152485aa59aee42673736d80e50d6ce
cooksey <greg@zello.com>
2021-04-29 16:41:22 -0500

    Updates Android demo app to load sdk from Maven Central (#141)
    * Removes stale SDK sources from this repository
    * Updates build tools

--- sdks/android/sdk/channel-sdk/src/main/cpp/amplifier.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define EQUALITY_COEF 1000
-#define EQUALITY_COEF_FL 1000.0f
-
-int transformAmplifierGainToCoef(int iAmplifierGain);
-void doAmplification(jshort* pDest,short* pSrc,int nOutput,int iAmplifierCoef);
-
